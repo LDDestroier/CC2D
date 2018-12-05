575e64462886b5ba64cd927a42b044280924dd84
LDDestroier <etheilig02@gmail.com>
2018-12-04 23:07:24 -0500

    Added framebuffer

--- game.lua
+++ game.lua
@@ -1,3 +1,325 @@
+local class = function(base)
+	local c = { }    -- a new class instance
+	if type(base) == 'table' then
+		-- our new class is a shallow copy of the base class!
+		for i,v in pairs(base) do
+			c[i] = v
+		end
+		c._base = base
+	end
+	-- the class will be the metatable for all its objects,
+	-- and they will look up their methods in it.
+	c.__index = c
+
+	-- expose a constructor which can be called by <classname>(<args>)
+	setmetatable(c, {
+		__call = function(class_tbl, ...)
+			local obj = { }
+			setmetatable(obj,c)
+			if class_tbl.init then
+				class_tbl.init(obj, ...)
+			else
+				-- make sure that any stuff from the base class is initialized!
+				if base and base.init then
+					base.init(obj, ...)
+				end
+			end
+			return obj
+		end
+	})
+
+	c.is_a =
+		function(self, klass)
+			local m = getmetatable(self)
+			while m do
+				if m == klass then return true end
+				m = m._base
+			end
+			return false
+		end
+	return c
+end
+
+local _rep = string.rep
+local _sub = string.sub
+local _gsub = string.gsub
+local colors = _G.colors
+
+local Canvas = class()
+
+Canvas.colorPalette = { }
+Canvas.darkPalette = { }
+Canvas.grayscalePalette = { }
+
+for n = 1, 16 do
+	Canvas.colorPalette[2 ^ (n - 1)]     = _sub("0123456789abcdef", n, n)
+	Canvas.grayscalePalette[2 ^ (n - 1)] = _sub("088888878877787f", n, n)
+	Canvas.darkPalette[2 ^ (n - 1)]      = _sub("8777777f77fff77f", n, n)
+end
+
+function Canvas:init(args)
+	self.x = 1
+	self.y = 1
+
+	for k,v in pairs(args) do
+		self[k] = v
+	end
+
+	self.ex = self.x + self.width - 1
+	self.ey = self.y + self.height - 1
+
+	if not self.palette then
+		if self.isColor then
+			self.palette = Canvas.colorPalette
+		else
+			self.palette = Canvas.grayscalePalette
+		end
+	end
+
+	self.lines = { }
+	for i = 1, self.height do
+		self.lines[i] = { }
+	end
+end
+
+function Canvas:resize(w, h)
+	for i = self.height, h do
+		self.lines[i] = { }
+	end
+
+	while #self.lines > h do
+		table.remove(self.lines, #self.lines)
+	end
+
+	if w ~= self.width then
+		for i = 1, self.height do
+			self.lines[i] = { dirty = true }
+		end
+	end
+
+	self.ex = self.x + w - 1
+	self.ey = self.y + h - 1
+	self.width = w
+	self.height = h
+end
+
+function Canvas:write(x, y, text, bg, fg)
+	if bg then
+		bg = _rep(self.palette[bg], #text)
+	end
+	if fg then
+		fg = _rep(self.palette[fg], #text)
+	end
+	self:writeBlit(x, y, text, bg, fg)
+end
+
+function Canvas:writeBlit(x, y, text, bg, fg)
+	if y > 0 and y <= #self.lines and x <= self.width then
+		local width = #text
+
+		-- fix ffs
+		if x < 1 then
+			text = _sub(text, 2 - x)
+			if bg then
+				bg = _sub(bg, 2 - x)
+			end
+			if bg then
+				fg = _sub(fg, 2 - x)
+			end
+			width = width + x - 1
+			x = 1
+		end
+
+		if x + width - 1 > self.width then
+			text = _sub(text, 1, self.width - x + 1)
+			if bg then
+				bg = _sub(bg, 1, self.width - x + 1)
+			end
+			if bg then
+				fg = _sub(fg, 1, self.width - x + 1)
+			end
+			width = #text
+		end
+
+		if width > 0 then
+
+			local function replace(sstr, pos, rstr, width)
+				if pos == 1 and width == self.width then
+					return rstr
+				elseif pos == 1 then
+					return rstr .. _sub(sstr, pos+width)
+				elseif pos + width > self.width then
+					return _sub(sstr, 1, pos-1) .. rstr
+				end
+				return _sub(sstr, 1, pos-1) .. rstr .. _sub(sstr, pos+width)
+			end
+
+			local line = self.lines[y]
+			line.dirty = true
+			line.text = replace(line.text, x, text, width)
+			if fg then
+				line.fg = replace(line.fg, x, fg, width)
+			end
+			if bg then
+				line.bg = replace(line.bg, x, bg, width)
+			end
+		end
+	end
+end
+
+function Canvas:writeLine(y, text, fg, bg)
+	self.lines[y].dirty = true
+	self.lines[y].text = text
+	self.lines[y].fg = fg
+	self.lines[y].bg = bg
+end
+
+function Canvas:reset()
+	self.regions = nil
+end
+
+function Canvas:clear(bg, fg)
+	local text = _rep(' ', self.width)
+	fg = _rep(self.palette[fg or colors.white], self.width)
+	bg = _rep(self.palette[bg or colors.black], self.width)
+	for i = 1, self.height do
+		self:writeLine(i, text, fg, bg)
+	end
+end
+
+function Canvas:redraw(device)
+	self:reset()
+	self:blit(device)
+	self:clean()
+end
+
+function Canvas:isDirty()
+	for _, line in pairs(self.lines) do
+		if line.dirty then
+			return true
+		end
+	end
+end
+
+function Canvas:dirty()
+	for _, line in pairs(self.lines) do
+		line.dirty = true
+	end
+end
+
+function Canvas:clean()
+	for _, line in pairs(self.lines) do
+		line.dirty = false
+	end
+end
+
+function Canvas:render(device) --- redrawAll ?
+	self:blit(device)
+	self:clean()
+end
+
+function Canvas:blit(device, src, tgt)
+	src = src or { x = 1, y = 1, ex = self.ex - self.x + 1, ey = self.ey - self.y + 1 }
+	tgt = tgt or self
+
+	for i = 0, src.ey - src.y do
+		local line = self.lines[src.y + i]
+		if line and line.dirty then
+			local t, fg, bg = line.text, line.fg, line.bg
+			if src.x > 1 or src.ex < self.ex then
+				t  = _sub(t, src.x, src.ex)
+				fg = _sub(fg, src.x, src.ex)
+				bg = _sub(bg, src.x, src.ex)
+			end
+			--if tgt.y + i > self.ey then -- wrong place to do clipping ??
+			--  break
+			--end
+			device.setCursorPos(tgt.x, tgt.y + i)
+			device.blit(t, fg, bg)
+		end
+	end
+end
+
+function Canvas:applyPalette(palette)
+
+	local lookup = { }
+	for n = 1, 16 do
+		lookup[self.palette[2 ^ (n - 1)]] = palette[2 ^ (n - 1)]
+	end
+
+	for _, l in pairs(self.lines) do
+		l.fg = _gsub(l.fg, '%w', lookup)
+		l.bg = _gsub(l.bg, '%w', lookup)
+		l.dirty = true
+	end
+
+	self.palette = palette
+end
+
+function Canvas.convertWindow(win, parent, wx, wy)
+	local w, h = win.getSize()
+
+	win.canvas = Canvas({
+		x       = wx,
+		y       = wy,
+		width   = w,
+		height  = h,
+		isColor = win.isColor(),
+	})
+
+	function win.clear()
+		win.canvas:clear(win.getBackgroundColor(), win.getTextColor())
+	end
+
+	function win.clearLine()
+		local _, y = win.getCursorPos()
+		win.canvas:write(1,
+			y,
+			_rep(' ', win.canvas.width),
+			win.getBackgroundColor(),
+			win.getTextColor())
+	end
+
+	function win.write(str)
+		local x, y = win.getCursorPos()
+		win.canvas:write(x,
+			y,
+			str,
+			win.getBackgroundColor(),
+			win.getTextColor())
+		win.setCursorPos(x + #str, y)
+	end
+
+	function win.blit(text, fg, bg)
+		local x, y = win.getCursorPos()
+		win.canvas:writeBlit(x, y, text, bg, fg)
+	end
+
+	function win.redraw()
+		win.canvas:redraw(parent)
+	end
+
+	function win.scroll(n)
+		table.insert(win.canvas.lines, table.remove(win.canvas.lines, 1))
+		win.canvas.lines[#win.canvas.lines].text = _rep(' ', win.canvas.width)
+		win.canvas:dirty()
+	end
+
+	function win.reposition(x, y, width, height)
+		win.canvas.x, win.canvas.y = x, y
+		win.canvas:resize(width or win.canvas.width, height or win.canvas.height)
+	end
+
+	win.clear()
+end
+
+local currTerm = term.current()
+local w, h = term.getSize()
+local win = window.create(currTerm, 1, 1, w, h, true)
+Canvas.convertWindow(win, currTerm, 1, 1)
+
+term = win
+
 local game = {}
 game.path = fs.combine(fs.getDir(shell.getRunningProgram()),"data")
 game.apiPath = fs.combine(game.path, "api")
@@ -14,12 +336,6 @@ local scrollY = 0
 local killY = 100
 
 local keysDown = {}
-
-local tsv = function(visible)
-	if term.current().setVisible then
-		term.current().setVisible(visible)
-	end
-end
 
 local getAPI = function(apiName, apiPath, apiURL, doDoFile)
 	apiPath = fs.combine(game.apiPath, apiPath)
@@ -400,7 +716,6 @@ local moveTick = function()
 end
 
 local render = function()
-	tsv(false)
 	term.clear()
 	nfte.drawImage(maps[mapname], -scrollX + 1, -scrollY + 1)
 	for num,player in pairs(players) do
@@ -447,7 +762,6 @@ local render = function()
 			)
 		end
 	end
-	tsv(true)
 end
 
 -- determines what sprite a player uses
@@ -514,6 +828,7 @@ end
 
 local main = function()
 	while true do
+		win.redraw()
 		players[you].control = deriveControls(keysDown)
 		moveTick()
 		players[you].sprite = determineSprite(players[you])
